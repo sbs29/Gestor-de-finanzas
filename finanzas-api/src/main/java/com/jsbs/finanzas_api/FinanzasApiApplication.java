697b1b11af0942b1bb46dfc2381a87089d2fa410
sbs29 <juanse2929@gmail.com>
2026-05-10 17:03:17 +0200

    Añadimos el proyecto

---
+++ finanzas-api/src/main/java/com/jsbs/finanzas_api/FinanzasApiApplication.java
@@ -0,0 +1,13 @@
+package com.jsbs.finanzas_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanzasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanzasApiApplication.class, args);
+	}
+
+}
---
+++ finanzas-api/src/test/java/com/jsbs/finanzas_api/FinanzasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsbs.finanzas_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanzasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
