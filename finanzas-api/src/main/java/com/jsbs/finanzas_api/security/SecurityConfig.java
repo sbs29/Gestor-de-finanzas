99bb41303f3785e1126e639987f531279cf33b65
sbs29 <juanse2929@gmail.com>
2026-06-05 14:17:24 +0200

    fix docker cors configuration

--- finanzas-api/src/main/java/com/jsbs/finanzas_api/security/SecurityConfig.java
+++ finanzas-api/src/main/java/com/jsbs/finanzas_api/security/SecurityConfig.java
@@ -50,9 +50,14 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOriginPatterns(List.of(
+                "http://localhost:5173",
+                "http://127.0.0.1:5173"
+        ));
+
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setExposedHeaders(List.of("Authorization"));
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
